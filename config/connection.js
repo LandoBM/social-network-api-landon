2403da13b17c60a00e0b45d4ae04515e211ce6da
Landon Murray <landonbmurray@icloud.com>
2023-03-02 23:20:57 -0500

    added User model

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+// Export connection 
+module.exports = mongoose.connection;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+// Define Mongoose
+// const mongoose = require('mongoose')
+const {Schema, model} = require('mongoose')
+const validateEmail = function(email) {
+    let mail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    return mail.test(email)
+}
+
+
+// Create a new instance of Mongoose schema to define shape of each document
+const userSchema = new Schema(
+    {
+    username: {type: String, 
+        required: true, 
+        unique: true, 
+        trim: true
+    }},
+    {
+    email: {type: String, 
+        require: 'Email is required', 
+        unique: true,
+        validate: [validateEmail, 'Enter a valid email address'],
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Enter a valid email address']
+    }},
+    {
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref:'Thoughts'
+    }]},
+    {
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }]},
+    {
+        toJSON: {
+            virtual: true,
+            getters: true 
+        },
+        id: false
+    }
+)
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(() => {
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+module.exports = User
